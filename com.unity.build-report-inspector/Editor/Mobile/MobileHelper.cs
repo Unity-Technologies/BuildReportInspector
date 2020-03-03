0f21d6131db9b0fbe4a309213277ed691d1ced0f
Vaidas Budrys <vaidas.budrys3@gmail.com>
2020-03-03 16:04:46 +0200

    Refactor MobileHelper appendix generation

--- com.unity.build-report-inspector/Editor/Mobile/MobileHelper.cs
+++ com.unity.build-report-inspector/Editor/Mobile/MobileHelper.cs
@@ -24,39 +24,46 @@ namespace Unity.BuildReportInspector.Mobile
 
         internal static void GenerateAndroidAppendix(string applicationPath, string guid)
         {
-            var mobileAppendix = new MobileAppendix(applicationPath);
-            SaveMobileAppendix(mobileAppendix, guid);
+            GenerateMobileAppendix(applicationPath, guid);
         }
 
         internal static void GenerateAppleAppendix(string applicationPath, string guid)
         {
-            using (var archive = ZipFile.OpenRead(applicationPath))
+            try
             {
-                var guidFile = archive.Entries.FirstOrDefault(x => x.Name == "UnityBuildGuid.txt");
-                if (guidFile == null)
+                using (var archive = ZipFile.OpenRead(applicationPath))
                 {
-                    Debug.LogError("The provided application was built before BuildReportInspector package was added to the project.");
-                    return;
-                }
-
-                using (var reader = new StreamReader(guidFile.Open()))
-                {
-                    var applicationGuid = reader.ReadToEnd();
-                    if (applicationGuid != guid)
+                    var guidFile = archive.Entries.FirstOrDefault(x => x.Name == "UnityBuildGuid.txt");
+                    if (guidFile == null)
                     {
-                        Debug.LogError("The GUID of the selected report does not match the GUID of the provided application.\n" +
-                                       $"Expected: {guid} but got: {applicationGuid}.");
+                        Debug.LogError("The provided application was built before BuildReportInspector package was added to the project.");
                         return;
                     }
-                }
 
-                var mobileAppendix = new MobileAppendix(applicationPath);
-                SaveMobileAppendix(mobileAppendix, guid);
+                    using (var reader = new StreamReader(guidFile.Open()))
+                    {
+                        var applicationGuid = reader.ReadToEnd();
+                        if (applicationGuid != guid)
+                        {
+                            Debug.LogError("The GUID of the selected report does not match the GUID of the provided application.\n" +
+                                           $"Expected: {guid} but got: {applicationGuid}.");
+                            return;
+                        }
+                    }
+                }
+                
+                GenerateMobileAppendix(applicationPath, guid);
             }
+            catch
+            {
+                Debug.LogError("Could not open the application archive. Please provide a valid .ipa bundle.");
+            }
+
         }
 
-        private static void SaveMobileAppendix(MobileAppendix appendix, string guid)
+        private static void GenerateMobileAppendix(string applicationPath, string guid)
         {
+            var appendix = new MobileAppendix(applicationPath);
             var appendixFilePath = Path.Combine(AppendixSavePath, guid);
             appendix.Save(appendixFilePath);
         }
