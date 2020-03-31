1478394792bb1bf235814c99e452f5b1fcbd0ac3
Vaidas Budrys <vaidas.budrys3@gmail.com>
2020-03-31 14:19:54 +0300

    Try adding test folder

---
+++ com.unity.build-report-inspector/Tests/Editor/BuildReportInspectorTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BuildReportInspectorTests
+    {
+        [Test]
+        public void SimpleTest
+        {
+            Assert.That(false);
+        }
+    }
+}
